d7279630bd30589a01750d99a15d2af0700508f0
hassanabdisalan <hassanabdisalan3@gmail.com>
2024-06-21 09:26:03 -0700

    4 task

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: alphabet in lowercase except q and e
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c != 'q' && c != 'e')
+		{
+			putchar(c);
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
