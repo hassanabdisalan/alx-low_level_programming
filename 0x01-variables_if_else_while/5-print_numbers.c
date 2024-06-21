f1e9499d0631927d6831edc8ef826f13dbe6cfc4
hassanabdisalan <hassanabdisalan3@gmail.com>
2024-06-21 09:41:14 -0700

    num

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	char num;
+
+	for (num = '0'; num <= '9'; num++)
+	{
+		putchar(num);
+	}
+
+	putchar('\n');
+	return (0);
+}
