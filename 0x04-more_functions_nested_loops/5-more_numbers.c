792e82438a2d023bc54849ef57dd1027deca825a
hassanabdisalan <hassanabdisalan3@gmail.com>
2024-06-27 11:36:44 -0700

    5 more nubers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			if (j > 9)
+				_putchar('1');
+			_putchar('0' + (j % 10));
+		}
+		_putchar('\n');
+	}
+}
