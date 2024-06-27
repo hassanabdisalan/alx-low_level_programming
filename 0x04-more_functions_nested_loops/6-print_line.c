14920d8b2c10f53a86cdafa41dbb2ca05bdc5d59
hassanabdisalan <hassanabdisalan3@gmail.com>
2024-06-27 13:01:00 -0700

    6 print line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -2,14 +2,15 @@
 
 void print_line(int n)
 {
+	int i;
+
 	if (n <= 0)
 	{
 		_putchar('\n');
 	}
 	else
 	{
-		int 1;
-		for (int i = 0; i < n; i++)
+		for (i = 0; i < n; i++)
 		{
 			_putchar('_');
 		}
