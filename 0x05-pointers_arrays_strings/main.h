9f7ddaed630acb822bb17b6d10cec77efeb5da9a
hassanabdisalan <hassanabdisalan3@gmail.com>
2024-07-01 11:19:15 -0700

    strlenp

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,7 +2,6 @@
 #define MAIN_H
 
 void reset_to_98(int *n);
-int _putchar(char c);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
@@ -13,5 +12,6 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
+int _putchar(char c);
 
 #endif
