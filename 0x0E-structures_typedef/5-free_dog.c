678a49a84a997f33ea1f3f61e4c0f5a8b25f8d98
hassanabdisalan <hassanabdisalan3@gmail.com>
2024-07-22 21:50:39 -0700

    task 5

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - Frees dogs.
+ * @d: The dog to free.
+ */
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
