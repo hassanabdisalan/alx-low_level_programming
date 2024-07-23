6d1d12e5bc64f8ecb2e06942cce412669f2bfafa
hassanabdisalan <hassanabdisalan3@gmail.com>
2024-07-22 21:25:03 -0700

    task 3

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,8 @@
 #ifndef DOG_H
 #define DOG_H
 
+#include <stdio.h>
+
 /**
  * struct dog - A new type describing a dog.
  * @name: The name of the dog, type = char *
@@ -12,7 +14,8 @@ typedef struct dog
 	char *name;
 	float age;
 	char *owner;
-} dog_t;
+}dog_t;
+
 
 /* Function prototpe*/
 void init_dog(struct dog *d, char *name, float age, char *owner);
