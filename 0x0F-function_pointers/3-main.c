a275912467482feb04df77ec3485e761fc3137dd
hassanabdisalan <hassanabdisalan3@gmail.com>
2024-07-24 15:41:02 -0700

    Write a program that performs simple operations.

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,35 +1,47 @@
 #include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
-* main - Performs simple operations
-* @argc: Argument count
-* @argv: Argument vector
-* Return: 0 on success, otherwise exits with status 98, 99, or 100
-*/
+ * main - performs simple operations
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 on success, 98 on failure
+ */
 int main(int argc, char *argv[])
 {
-int num1, num2, result;
-int (*op_func)(int, int);
+	int a, b, result;
+	int (*op_func)(int, int);
 
-if (argc != 4)
-{
-printf("Error\n");
-exit(98);
+	/* check for correct number of arguments */
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	/* get numbers to operate on */
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+
+	/* check for division or mod by 0 */
+	if ((argv[2][0] == '/' || argv[2][0] == '%') && b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	/* get the appropriate function pointer */
+	op_func = get_op_func(argv[2]);
+	if (op_func == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	/* perform the operation */
+	result = op_func(a, b);
+	printf("%d\n", result);
+
+	return (0);
 }
-
-num1 = atoi(argv[1]);
-num2 = atoi(argv[3]);
-op_func = get_op_func(argv[2]);
-
-if (op_func == NULL || argv[2][1] != '\0')
-{
-printf("Error\n");
-exit(99);
-}
-
-result = op_func(num1, num2);
-printf("%d\n", result);
-
-return (0);
-}
-
