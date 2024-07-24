df04afee6dd6ff7f17765f43c2d11cb8690288a4
hassanabdisalan <hassanabdisalan3@gmail.com>
2024-07-24 15:27:39 -0700

    task 3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -21,7 +21,7 @@ num1 = atoi(argv[1]);
 num2 = atoi(argv[3]);
 op_func = get_op_func(argv[2]);
 
-if (op_func == NULL)
+if (op_func == NULL || argv[2][1] != '\0')
 {
 printf("Error\n");
 exit(99);
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,67 +1,65 @@
 #include "3-calc.h"
 
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+
 /**
-* op_add - returns the sum of a and b
-* @a: first integer
-* @b: second integer
-* Return: sum of a and b
-*/
+ * op_add - Returns the sum of two numbers.
+ * @a: The first number.
+ * @b: The second number.
+ *
+ * Return: The sum of a and b.
+ */
 int op_add(int a, int b)
 {
-return (a + b);
+	return (a + b);
 }
-
 /**
-* op_sub - returns the difference of a and b
-* @a: first integer
-* @b: second integer
-* Return: difference of a and b
-*/
+ * op_sub - Returns the difference of two numbers.
+ * @a: The first number.
+ * @b: The second number.
+ *
+ * Return: The difference of a and b.
+ */
 int op_sub(int a, int b)
 {
-return (a - b);
+	return (a - b);
 }
-
 /**
-* op_mul - returns the product of a and b
-* @a: first integer
-* @b: second integer
-* Return: product of a and b
-*/
+ * op_mul - Returns the product of two numbers.
+ * @a: The first number.
+ * @b: The second number.
+ *
+ * Return: The product of a and b.
+ */
 int op_mul(int a, int b)
 {
-return (a * b);
+	return (a * b);
 }
-
 /**
-* op_div - returns the result of the division of a by b
-* @a: first integer
-* @b: second integer
-* Return: result of the division of a by b
-*/
+ * op_div - Returns the division of two numbers.
+ * @a: The first number.
+ * @b: The second number.
+ *
+ * Return: The quotient of a and b.
+ */
 int op_div(int a, int b)
 {
-if (b == 0)
-{
-printf("Error\n");
-exit(100);
+	return (a / b);
 }
-return (a / b);
-}
-
 /**
-* op_mod - returns the remainder of the division of a by b
-* @a: first integer
-* @b: second integer
-* Return: remainder of the division of a by b
-*/
+ * op_mod - Returns the remainder of the division of two numbers.
+ * @a: The first number.
+ * @b: The second number.
+ *
+ * Return: The remainder of the division of a by b.
+ */
 int op_mod(int a, int b)
 {
-if (b == 0)
-{
-printf("Error\n");
-exit(100);
+	return (a % b);
 }
-return (a % b);
-}
+
 
