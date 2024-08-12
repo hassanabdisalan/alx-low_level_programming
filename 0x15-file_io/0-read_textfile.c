da4fa15425f0f3a85ea10268aa64117f08d17568
hassanabdisalan <hassanabdisalan3@gmail.com>
2024-08-12 11:22:15 -0700

    task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,42 +1,43 @@
 #include "main.h"
-/**
- * read_textfile -Reads and prints text from a file to stdout.
- * @filename:Name of the file.
- * @letters:Number of letters to be read and printed.
- * Return:Actual number of read and printed letters.
- */
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
 
+/**
+* read_textfile - Reads a text file and prints it to the POSIX standard output.
+* @filename: The name of the file to read.
+* @letters: The number of letters to read and print.
+*
+* Return: The actual number of letters it could read and print,
+* or 0 on failure.
+*/
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	char *s;
-	ssize_t rsize, wsize;
-
-	if (!filename)
-		return (0);
-	fd = open(filename, O_RDONLY);
-	if (fd < 0)
-		return (0);
-
-	s = malloc(sizeof(char) * letters);
-	if (!s)
-		return (0);
-	rsize = read(fd, s, letters);
-	if (rsize < 0)
-	{
-		free(s);
-		return (0);
-	}
-
-	s[rsize] = '\0';
-	close(fd);
-
-	wsize = write(STDOUT_FILENO, s, rsize);
-	if (wsize < 0)
-	{
-		free(s);
-		return (0);
-	}
-	free(s);
-	return (wsize);
+int fd;
+ssize_t rsize, wsize;
+char *s;
+if (!filename)
+return (0);
+fd = open(filename, O_RDONLY);
+if (fd == -1)
+return (0);
+s = malloc(sizeof(char) * letters);
+if (!s)
+{
+close(fd);
+return (0);
+}
+rsize = read(fd, s, letters);
+if (rsize == -1)
+{
+free(s);
+close(fd);
+return (0);
+}
+wsize = write(STDOUT_FILENO, s, rsize);
+free(s);
+close(fd);
+if (wsize != rsize)
+return (0);
+return (wsize);
 }
